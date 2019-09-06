3b544e096587c0a804f7a9244b6be82a5d138344
salahmak <zefeni@myzat.com>
2019-09-07 00:59:53 +0100

    Added local storage, The app is more flexible, Added a refraction file for functions, Added checkbox, Improved the html layouts.

---
+++ refraction.js
@@ -0,0 +1,43 @@
+//Read and parse existing data in local storage
+const getSavedTodos = function() {
+    const todoJSON = localStorage.getItem('todo');
+
+    if (todoJSON !== null) {
+    return JSON.parse(todoJSON)
+    } else {
+        return [];
+    }
+}
+
+// save the todos in locale storage
+const saveTodos = function(todos) {
+    localStorage.setItem('todo', JSON.stringify(todos))
+}
+
+// Render todos 
+let renderTodos = function(todos, filters) {
+    let filteredTodo = todos.filter(function(todo){
+        return todo.text.toLowerCase().includes(filters.text.toLowerCase());
+    })
+
+    filteredTodo = todos.filter(function(todo){
+        return !filters.hideCompleted || !todo.completed;
+    })
+
+    document.querySelector('#todo').innerHTML = '';
+
+    const incompleteTodos = filteredTodo.filter(function (todos) {
+        return !todos.completed
+    })
+
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todo').appendChild(summary)
+
+    filteredTodo.forEach(function(todo){
+        let p = document.createElement('p');
+        p.textContent = todo.text;
+        document.querySelector('#todo').appendChild(p);
+    })
+}
--- todo-app.js
+++ todo-app.js
@@ -1,48 +1,10 @@
-const todos = [{
-    text: 'wake up',
-    completed: true
-}, {
-    text: 'get some food',
-    completed: true
-}, {
-    text: 'play csgo',
-    completed: false
-}, {
-    text: 'play minecraft',
-    completed: true
-}, {
-    text: 'learn javascript',
-    completed: false
-}];
+let todos = getSavedTodos();
 
 
 
 let filters = {
-    text: ''
-}
-
-
-
-let renderTodos = function(todos, filters) {
-    let filteredTodo = todos.filter(function(todo){
-        return todo.text.toLowerCase().includes(filters.text.toLowerCase());
-    })
-    document.querySelector('#todo').innerHTML = '';
-
-    const incompleteTodos = filteredTodo.filter(function (todos) {
-        return !todos.completed
-    })
-
-
-    const summary = document.createElement('h2')
-    summary.textContent = `You have ${incompleteTodos.length} todos left`
-    document.querySelector('#todo').appendChild(summary)
-
-    filteredTodo.forEach(function(todo){
-        let p = document.createElement('p');
-        p.textContent = todo.text;
-        document.querySelector('#todo').appendChild(p);
-    })
+    text: '',
+    hideCompleted: false
 }
 
 renderTodos(todos, filters);
@@ -58,10 +20,12 @@ document.querySelector('#form').addEventListener('submit', function(e){
         text: e.target.elements.firstName.value,
         completed: false
     })
+    saveTodos(todos);
     renderTodos(todos, filters);
     e.target.elements.firstName.value = '';
 })
 
-document.querySelector('#checkbox').addEventListener('change', function(e){
-    e.target.checked;   //the boolean the checks id the checkbox is true or false
+document.querySelector('#hide-completed').addEventListener('change', function(e){
+   filters.hideCompleted = e.target.checked;
+   renderTodos(todos, filters);
 })
